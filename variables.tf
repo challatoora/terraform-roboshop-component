ecdfd7a143bf679039694ae9203e0eedc8351400
challatoor murali <challatoor murali.gmail.com>
2026-03-25 20:31:46 +0530

    tf

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "component" {
+    type = string
+}
+
+variable "app_version" {
+    type = string
+    default = "v3"
+}
+
+variable "rule_priority" {
+    
+}
+
+variable "zone_id"{
+    default = "Z03767891OMNNRGY4CE9B"
+}
+
+variable "domain_name"{
+    default = "mreddy.online"
+}
